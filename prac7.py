320c688858d36d6d9fd7a16db885ac8426ca191c
LXXDOH001 <31177642+LXXDOH001@users.noreply.github.com>
2018-10-24 21:33:47 +0200

    Add files via upload

--- prac7.py
+++ prac7.py
@@ -2,17 +2,19 @@ import RPi.GPIO as GPIO
 import time
 import os
 import Adafruit_MCP3008
-import subprocess
-import sys
-import 
+
 #detect a falling signal
 #def checkWave():
 
+
 def main():
 	# Main function to sense a wave
-	global interval, light, mcp, counter
-	print("Expose to light for 5s!")
-	time.sleep(1)
+	global interval, light, mcp, log, speed, groupingNum, avgTime, avgFreq
+	flag = 0				# flag is to indicate the pulse has begun and timer is running
+	timer = 0				# measures time between the two pulses
+	counter = 0
+	temp = 0
+	print("Calculating average light value of the room fr 5s...")
 
 
 	#get an average value for light on (HIGH)
@@ -31,12 +33,11 @@ def main():
 
 	while(1):
 		
-		time.sleep(0.1)
+		time.sleep(0.01)	# minimal delay
 		final = mcp.read_adc(1) # channel 1 used
 
+		tolerance = 10 # tolerance for adc values to fluctuate
 		#print(final)
-		
-
 		if (final > (highVal + tolerance)):
 			os.system('clear')
 			print("Wave started")
@@ -44,37 +45,62 @@ def main():
 			while (final > (highVal + tolerance)):
 
 				#print(final)
-				time.sleep(0.05)
-				interval += 0.05
+				time.sleep(0.01)
+				interval += 0.01
 				final = mcp.read_adc(1)
+				if (flag == 0):
+					start = time.time()
+					flag = 1	# set flag
 
 				
-			counter += 1
-			Time.append(interval)
+
 			os.system('clear')
 			print("Wave ended")
 			print(str(interval) + "s")
-		
-		
-		
+			if (interval > 5):
+				break
+			log.append(round(interval, 4))
+			stop = time.time()
+			timer = round(stop - start, 4) 	# round off to 4 decimal points
+			speed.append(timer)
+			flag = 0			# reset flag
+			interval = 0
+			counter = counter + 1
+			print(counter)
 
-		interval = 0
+			temp = temp + timer
+			if (counter%groupingNum == 0):
+				avgTime.append(round(temp/groupingNum, 4))
+				avgFreq.append(round(groupingNum/temp, 4))
+				temp = 0		# reset temp
 
-		 		
+
+	print("Time of waves: ")
+	print(log)
+	print("Counter: ")
+	print(counter)
+	print("Time between waves: ")
+	print(speed)
+	print("Avgerage number of waves per second")
+	print(avgFreq)
 
 
 #Globals
 #GPIO.cleanup()
 print("Setting up")
-time.sleep(2)
 light = []
-Time[]	# Array of time taken of each wave
-speed[] = []# Array to work out speed of waves
-setup = []
+log = []		# stores the duration of the pulse
+speed = []		# stores the duration of the wave (shadow)
+counter = 0
+groupingNum = 10 	# user can set this (this is the number of waves to group for the average speed)
+avgTime = []		# average time between 2 waves
+avgFreq = []		# average waves in 1 second
+
+
 ch1 = [0]*16 # Channel for the knob
 GPIO.setmode(GPIO.BCM)
 interval = 0 # Time passed
-counter = 0
+
     # Software SPI configuration (in BCM mode):
 CLK  = 11
 MISO = 9
@@ -82,10 +108,7 @@ MOSI = 10
 CS   = 8
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
-#Server setup needs to be done
-for eachArg in sys.argv:
-	data
-f = open('logs.txt')
+#Server setup
 
 os.system("clear")
 if __name__ == '__main__':
