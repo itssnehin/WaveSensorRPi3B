a59c54e1c958d1f0f9027b0c56c2235cee834eda
LXXDOH001 <31177642+LXXDOH001@users.noreply.github.com>
2018-10-24 21:35:16 +0000

    Create server_1.py

---
+++ server_1.py
@@ -0,0 +1,84 @@
+import socket
+
+host = ''
+port = 5560
+
+storedValue = "You are connected to the raspberry pi"
+
+def setupServer():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket created.")
+    try:
+        s.bind((host, port))
+    except socket.error as msg:
+        print(msg)
+    print("Socket bind comlete.")
+    return s
+
+def setupConnection():
+    s.listen(1) # Allows one connection at a time.
+    conn, address = s.accept()
+    print("Connected to: " + address[0] + ":" + str(address[1]))
+    return conn
+
+def tolerance():
+    reply = "State new tolerance value: "
+    return reply
+
+def datalogs():
+    reply = "Printing the data logs of the duration of the wave in s: "
+    
+    return reply
+
+def start():
+    reply = "Starting application"
+    
+    return reply
+
+def stop():
+    reply = "Stopping application"
+    
+    return reply
+
+def shutdown():
+    reply = "Shutting down"
+
+def dataTransfer(conn):
+    # A big loop that sends/receives data until told not to.
+    while True:
+        # Receive the data
+        data = conn.recv(1024) # receive the data
+        data = data.decode('utf-8')
+        # Split the data such that you separate the command
+        # from the rest of the data.
+        dataMessage = data.split(' ', 1)
+        command = dataMessage[0]
+        if command == '1':
+            reply = tolerance()
+        elif command == '2':
+            reply = datalogs()
+        elif command == '3':
+            reply = start()
+        elif command == '4':
+            reply = stop()
+        elif command == '5':
+            reply = shutdown()
+            print("Our server is shutting down.")
+            s.close()
+            break
+        else:
+            reply = 'Unknown Command'
+        # Send the reply back to the client
+        conn.sendall(str.encode(reply))
+        print("Data has been sent!")
+    conn.close()
+        
+
+s = setupServer()
+
+while True:
+    try:
+        conn = setupConnection()
+        dataTransfer(conn)
+    except:
+        break
